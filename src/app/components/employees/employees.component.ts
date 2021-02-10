b3646692f301102a2b80336aa6cdaf84f97a3b6e
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2021-02-10 17:48:23 +0700

    Add search function with debounceTime and distinctUntilChanged.

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { EmployeeService } from '../../services/employee/employee.service';
 import { Employee } from '../../interfaces/employee';
 
@@ -9,6 +11,7 @@ import { Employee } from '../../interfaces/employee';
 })
 export class EmployeesComponent implements OnInit {
   employees: Employee[] = [];
+  searchTerms: Subject<string> = new Subject<string>();
 
   constructor(private employeeService: EmployeeService) { }
 
@@ -16,5 +19,20 @@ export class EmployeesComponent implements OnInit {
     this.employeeService.getEmployees().subscribe(employees => {
       this.employees = employees;
     });
+
+    this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged()
+
+    ).subscribe(term => {
+      console.log(term)
+    });
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 }
