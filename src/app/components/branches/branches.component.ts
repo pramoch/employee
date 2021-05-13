d4232beefd2d44bf6a0ff1347e0fc32fd842e41c
Pramoch Chulapong <pramoch.chulapong@gmail.com>
2021-05-13 17:34:30 +0700

    List branch

--- src/app/components/branches/branches.component.ts
+++ src/app/components/branches/branches.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { BranchService } from '../../services/branch/branch.service';
+import { DialogService } from '../../services/dialog/dialog.service';
+
 @Component({
   selector: 'app-branches',
   templateUrl: './branches.component.html',
   styleUrls: ['./branches.component.scss']
 })
 export class BranchesComponent implements OnInit {
+  branches: string[] = [];
 
-  constructor() { }
+  constructor(
+    private branchService: BranchService,
+    private dialog: DialogService
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.dialog.showLoading();
 
+    this.branchService.getBranches()
+      .subscribe(result => {
+        this.dialog.hideLoading();
+
+        if (result.status.success && result.data) {
+          this.branches = result.data.branches;
+        }
+      });
+  }
 }
